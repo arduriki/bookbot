5c20f29b52225070d12b84ea8b565a1774e10ed5
arduriki <jordi.ardura.professional@gmail.com>
2025-01-17 10:29:15 +0100

    function: count_characters created and tested

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from operator import le
+
+
 frankenstein_book = "books/frankenstein.txt"
 
 
@@ -19,8 +22,31 @@ def count_book_words(file):
     print(f"The document has {len(words)} words")
 
 
+def count_characters(file):
+    """Shows in console how many characters the file has
+    and how many times they're repeated
+    """
+    characters = "abcdefghijklmnopqrstuvwxyz"
+    num_char_dict = {}
+    # Create a dictionary with all the characters
+    for char in characters:
+        num_char_dict[char] = 0
+    # Lower the text
+    text = open_file(file)
+    lowered_text = text.lower()
+    # Increment the number of times a character appears
+    for letter in lowered_text:
+        if letter in characters:
+            num_char_dict[letter] += 1
+
+    print(
+        f"The number of times that different characters appear in the document\n{num_char_dict}"
+    )
+
+
 def main():
     count_book_words(frankenstein_book)
+    count_characters(frankenstein_book)
 
 
 main()
